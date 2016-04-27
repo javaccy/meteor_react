f4cbe29a968ed3e6ab882ae32e47442479c4b08f
ccy <javaccy@gmail.com>
2016-04-28 07:16:18 +0800

    添加路由　react-router

--- client/main.jsx
+++ client/main.jsx
@@ -4,9 +4,13 @@ import { Meteor } from 'meteor/meteor';
 
 import injectTapEventPlugin from 'react-tap-event-plugin'
 
+import { renderRoutes } from '../imports/startup/client/routes.jsx';
+
 import Hello from '../imports/ui/Hello.jsx';
 
 Meteor.startup(() => {
 	injectTapEventPlugin()
-  	render(<Hello />, document.getElementById('app-container'));
+  	//render(<Hello />, document.getElementById('app-container'));
+
+  	 render(renderRoutes(), document.getElementById('app-container'));
 });
