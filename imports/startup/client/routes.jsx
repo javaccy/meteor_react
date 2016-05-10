4bceeb8c2745865fede78f6ce709c8cb9c6106e9
ccy <javaccy@gmail.com>
2016-05-10 21:20:14 +0800

    定制导航条样式,添加radium

--- imports/startup/client/routes.jsx
+++ imports/startup/client/routes.jsx
@@ -4,15 +4,16 @@ import { Router,IndexRoute, Route, browserHistory } from 'react-router'
 import SignUp from '../../ui/SignUp.jsx';
 import LogIn from '../../ui/Login.jsx';
 import Home from '../../ui/Home.jsx';
-
+import Hello from '../../ui/Hello.jsx';
 import App from '../../ui/App.jsx';
 
 export const renderRoutes = () => (
-  <Router history={browserHistory}>
-  	<Route path='/' component={App}>
-	  	<IndexRoute path='/' component={Home} />
+  <Router path='/' history={browserHistory}>
+  	<Route component={App}>
+	  	<Route path='/' component={Home} />
 	    <Route path="/signup" component={SignUp} />
 	    <Route path="/login" component={LogIn} />
-  	</Route>    
+	    <Route path="/hello" component={Hello} />
+  	</Route>
   </Router>
 );
--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,28 +1,45 @@
 import React,{Component} from 'react';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
+
+import Radium from 'radium';
+
 import NavBar from './shared/NavBar.jsx';
 
-class App extends Component{
 
-	getChildContext() {
-		return {
-			muiTheme: getMuiTheme()
-		};
-	}
-	render(){
-		return(
+class App extends Component {
 
-				<div>
-					<NavBar/>
-					{this.props.children}
-				</div>
-			)
-	}
+    getChildContext() {
+        return {
+            muiTheme: getMuiTheme()
+        };
+    }
+
+    getStyles(){
+        return {
+            root:{
+                display:'flex',
+                flexDirection:'column',
+                height:'100vh',
+                minHeight:'100vh',
+            }
+        }
+    }
+
+    render() {
+        const styles = this.getStyles();
+        return (
+
+            <div style={styles.root}>
+                <NavBar/>
+                {this.props.children}
+            </div>
+        )
+    }
 
 }
 
 App.childContextTypes = {
-	muiTheme: React.PropTypes.object.isRequired,
+    muiTheme: React.PropTypes.object.isRequired,
 };
 
-export default App;
+export default Radium(App);
